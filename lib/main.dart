fb01be388e53d85c26976770879d9746b6e94447
Sujit Sarkar <snigdho532@gmail.com>
2022-05-18 02:45:33 +0600

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<PublicController>(
       builder: (pc) {
-        pc.toggleStatusBar();
+        pc.toggleStatusBar(); //statusBar Theme
         return GetMaterialApp(
           title: 'Cricland',
           debugShowCheckedModeBanner: false,
