1c6fdacbf0f4a4b5f04f479144bdeb9f8d9d7edf
Sujit Sarkar <snigdho532@gmail.com>
2022-10-11 19:39:00 +0600

    for pull

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'home/view/delete_screen.dart';
 
 void main() async {
