80eb1170680131282d9d364dee8c88f07cdaef32
Sujit Sarkar <snigdho532@gmail.com>
2023-02-06 01:52:38 +0600

    splash screen added

--- lib/public/screens/splash_screen.dart
+++ lib/public/screens/splash_screen.dart
@@ -1,7 +1,9 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:cricland/public/controller/language_controller.dart';
 import 'package:cricland/public/screens/home_nav_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import '../../home/model/custom_widget/constants.dart';
 import '../controller/public_controller.dart';
 import '../variables/config.dart';
@@ -29,7 +31,6 @@ class _SplashScreenState extends State<SplashScreen>
     _initData();
   }
 
-
   Future<void> _initData() async {
     Future.delayed(const Duration(seconds: 3))
         .then((value) => Get.offAll(() => const HomeNavPage()));
@@ -46,43 +47,36 @@ class _SplashScreenState extends State<SplashScreen>
     final LanguageController langController = Get.find();
     langController.initializeApp(context);
     return GetBuilder<PublicController>(
-      init: PublicController(),
-      builder: (pc) {
-      return Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: FadeTransition(
-            opacity: _animation,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.white,
-                  child: Image.asset('assets/dark_logo.png',
-                      width: dSize(.4),
-                      height: dSize(.4),
-                      fit: BoxFit.cover),
+        init: PublicController(),
+        builder: (pc) {
+          return Scaffold(
+              backgroundColor: Colors.white,
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Lottie.asset('assets/lottie/splash_lottie.json'),
+                    AnimatedTextKit(
+                      animatedTexts: [
+                        TypewriterAnimatedText(
+                          'Crickland',
+                          textStyle: TextStyle(
+                              fontSize: dSize(.09),
+                              fontWeight: FontWeight.w900,
+                              color: const Color(0xffFF002D)),
+                          speed: const Duration(milliseconds: 100),
+                        ),
+                      ],
+                      totalRepeatCount: 100,
+                      pause: const Duration(milliseconds: 1000),
+                      displayFullTextOnTap: true,
+                      stopPauseOnTap: true,
+                    ),
+                  ],
                 ),
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                      style: CLTextStyle().paragraphHeadLineTextStyle.copyWith(
-                          color: const Color(0xffFF002D),
-                          fontSize: dSize(.09),
-                          fontWeight: FontWeight.w900
-                      ),
-
-                    children: const <TextSpan>[
-                      TextSpan(text: 'Crickland'),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      );
-    });
+              )
+              );
+        });
   }
 }
